6c5ba64ace061e8a364ca214f91570d01382e020
arrorobbi <vickyrobbi@gmail.com>
2024-02-21 08:40:25 +0700

    chore: modifying gitignore and add env.example

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser')
 const routes = require('./routers/index')
@@ -14,8 +13,7 @@ app.use('/uploads', express.static("upload"))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
