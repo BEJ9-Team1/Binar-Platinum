0269db30979f1d6f112aa8b314c73b1f574dcbe0
arrorobbi <vickyrobbi@gmail.com>
2024-02-24 11:06:43 +0700

    feat: add router address

--- routers/user_route.js
+++ routers/user_route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const categoryController = require('../controllers/category_controller')
 const paymentController = require('../controllers/payment_controller')
+const AddressController = require('../controllers/address_controller')
+
 // USER CONTROLLER //
 
 
@@ -12,7 +14,14 @@ router.patch('/category/:id', categoryController.update)
 router.get('/category/:name', categoryController.find)
 router.delete('/category/:id', categoryController.destroy)
 
+//ADDRESS//
+router.get('/address', AddressController.index)
+router.post('/address', AddressController.create)
+router.get('/address/:id', AddressController.find)
+router.put('/address/:id', AddressController.update)
+router.delete('/address/:id', AddressController.destroy)
 
+//PAYMENT//
 router.get('/payment', paymentController.index)
 router.post('/payment', paymentController.create)
 router.get('/payment/:name', paymentController.find)
