c67fdb48832a4dc91f254c4c9e12c6a14638e58e
luckylaurens15 <lucky.laurens@binus.ac.id>
2024-03-09 10:30:32 +0700

    feat: migration and models for cart and cartItem

---
+++ models/cart.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Cart.hasMany(models.CartItem, {
+        foreignKey: "cartId",
+        sourceKey: "id",
+      });
+    }
+  }
+  Cart.init({
+    userId: DataTypes.UUID
+  }, {
+    sequelize,
+    modelName: 'Cart',
+  });
+  return Cart;
+};
