4dbe585a0f4cc0ce78e2dd34b86b6ade85756d43
arrorobbi <vickyrobbi@gmail.com>
2024-03-14 21:26:20 +0700

    add migrations

---
+++ migrations/20240223071632-create-address.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Addresses', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      userId: {
+        type: Sequelize.UUID,
+        references: {
+          key: "id",
+          model: "Users"
+        },
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isUsed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Addresses');
+  }
+};
