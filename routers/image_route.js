3f6aaa6a93c18fcea5040ee649c2e11c8bdd4efb
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-03-20 12:10:24 +0700

    replace router with the new one

---
+++ routers/image_route.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const mediaController = require('../controllers/media_controller');
+const { upload } = require('../services/media_services');
+const  {JWTAuth} = require('../middlewares/auth-jwt')
+const {RoleGuard} = require('../middlewares/role-guard')
+
+//MEDIA
+router.get('/media', mediaController.index)
+router.get('/media/:id',mediaController.find)
+router.post('/media', JWTAuth, RoleGuard('buyer', 'merchant'), upload.single("picture"),mediaController.create)
+router.put('/media/:id', JWTAuth, RoleGuard('buyer', 'merchant'), upload.single("picture"),mediaController.updateImage)
+router.delete('/media/:id', JWTAuth, RoleGuard('buyer', 'merchant'),mediaController.destroy)
+
+module.exports= router
